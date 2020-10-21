b8aaca201f06525ff7514ad2e18ff96a407b3747
Bishakha Roy <seroy417@gmail.com>
2020-10-21 17:53:53 -0400

    Created course Controller, and stated the index for courses

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ get "courses" => "courses#index"
+ get "courses/new" => "courses#new"
+ post "courses" => "courses#create"
 end
