ad4fd277dc883b55fd61410e8079faeefb9df56a
Sean Plunkett <seanplunkett474@gmail.com>
2020-10-22 10:40:04 -0400

    Finished student index and edit. Student show is done awaiting courses model.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,38 @@
 class StudentsController < ApplicationController
 	def index
-		
+		@students = Student.all
+	end
+
+	def show
+		@courses = student.find(params[:id]).courses
+	end
+
+	def edit
+		@student = Student.find(params[:id])
+	end
+
+	def create
+		student = Student.new(student_params)
+		if student.save
+			redirect_to "/student"
+		else
+			flash[:errors] = student.errors.full_messages
+			redirect_to "/student/new"
+		end
+	end
+
+	def update
+		student = Student.find(params[:id])
+		if student.update(student_params)
+			redirect_to "/students"
+		else
+			flash[:errors] = studentstudent.errors.full_messages
+			redirect_to "/students/#{student.id}/edit"
+		end
+	end
+
+	private 
+	def student_params
+		params.require(:student).permit(:first_name, :last_name)
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
  	get "courses" => "courses#index"
 	get "courses/new" => "courses#new"
+ 	
  	post "courses" => "courses#create"
 	get "students" => "students#index"
+
+	get "students/:id/edit" => "students#edit"
+	patch "students/:id" => "students#update"
+	delete "students/:id" => "students#destroy"
 end
