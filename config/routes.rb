32061fcdbb47e1c786b1c73d345b258dffca6492
Sean Plunkett <seanplunkett474@gmail.com>
2020-10-22 10:04:47 -0400

    Fix routes and continued creating model for student

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
- get "courses" => "courses#index"
- get "courses/new" => "courses#new"
- post "courses" => "courses#create"
-=======
+ 	get "courses" => "courses#index"
+	get "courses/new" => "courses#new"
+ 	post "courses" => "courses#create"
 	get "students" => "students#index"
->>>>>>> origin/main
 end
