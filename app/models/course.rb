47a0af5061c71942fd876c1e55724c1f5fa832db
Sean Plunkett <seanplunkett474@gmail.com>
2020-10-29 09:32:38 -0400

    create the studentCourse model to create bridge between Student and Course.

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,5 @@
 class Course < ApplicationRecord
+	has_many :studentCourse
+	has_many :students, through: :studentCourse
 	validates :title, :professor, :room, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201022213902) do
+ActiveRecord::Schema.define(version: 20201029131754) do
 
   create_table "courses", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,15 @@ ActiveRecord::Schema.define(version: 20201022213902) do
     t.string "room"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "student_courses", force: :cascade do |t|
+    t.integer "student_id"
+    t.integer "course_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_student_courses_on_course_id"
+    t.index ["student_id"], name: "index_student_courses_on_student_id"
   end
 
   create_table "students", force: :cascade do |t|
