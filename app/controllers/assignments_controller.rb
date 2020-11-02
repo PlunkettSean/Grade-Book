f4031caa2e2a2c0b54d6e75c985f199e2b3d80d7
Bishakha Roy <seroy417@gmail.com>
2020-11-02 02:23:07 -0500

    Created Assignment's edit, index, new and the controller

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,2 +1,47 @@
 class AssignmentsController < ApplicationController
+def index
+		@assignments = Assignment.all
+	end
+
+	def new
+	end
+
+	def create
+		assignment = Assignment.new(course_params)
+		if assignment.save
+			redirect_to "/assignments"
+		else
+			flash[:errors] = assignment.errors.full_messages
+			redirect_to "/assignments/new"
+		end
+	end
+
+	def show
+		@students = Assignment.find(params[:id]).students
+	end 
+
+	def edit
+		@assignment = Assignment.find(params[:id])
+	end
+
+	def update
+		assignment = Assignment.find(params[:id])
+		if assignment.update(assignment_params)
+			redirect_to "/assignments"
+		else
+			flash[:errors] = assignment.errors.full_messages
+			redirect_to "/assignments/#{assignment.id}/edit"
+		end
+	end
+
+	def destroy
+		assignment = Assignment.find(params[:id])
+		assignment.destroy
+		redirect_to "/assignments"
+	end
+
+	private
+	def assignment_params
+		params.require(:assignment).permit(:name, :description)
+	end
 end
