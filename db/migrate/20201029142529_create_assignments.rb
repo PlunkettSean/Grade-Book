164a5bde16e222b0533a0f3e33165be39242772f
Sean Plunkett <seanplunkett474@gmail.com>
2020-11-05 10:20:17 -0500

    Removed Temp

--- db/migrate/20201029142529_create_assignments.rb
+++ db/migrate/20201029142529_create_assignments.rb
@@ -1,6 +1,5 @@
 class CreateAssignments < ActiveRecord::Migration[5.1]
   def change
-  	drop_table :assignments
     create_table :assignments do |t|
       t.string :name
       t.string :description
