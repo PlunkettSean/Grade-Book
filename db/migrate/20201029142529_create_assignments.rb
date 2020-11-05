7f106661292a510888dc1c210b041aa3ebd4f035
Sean Plunkett <seanplunkett474@gmail.com>
2020-11-05 10:16:20 -0500

    Atempt fix

--- db/migrate/20201029142529_create_assignments.rb
+++ db/migrate/20201029142529_create_assignments.rb
@@ -1,5 +1,6 @@
 class CreateAssignments < ActiveRecord::Migration[5.1]
   def change
+  	drop_table :assignments
     create_table :assignments do |t|
       t.string :name
       t.string :description
